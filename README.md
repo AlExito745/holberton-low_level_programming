5cc420d536b87101d4fad439670e0bac7ea9cbab
AlExito745 <alxtinoco1988@gmail.com>
2022-10-11 09:44:20 -0700

    C-tolls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 C-Hello, World
+C - Variables, if, else, while
+C - Functions, nested loops
+C - More functions, more nested loops
+C - Pointers, arrays and strings
---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reser_to_98(int *n) - Update the value it points
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
+
