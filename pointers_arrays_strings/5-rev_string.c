3c9e44a292ec2f1e6d4c4819c2d5dc4e9695e213
AlExito745 <alxtinoco1988@gmail.com>
2022-10-19 04:17:54 -0700

    Useful makefile2

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Description: changes position a character in string
+ * Return: nothing
+ **/
+
+void rev_string(char *s)
+{
+	
+}
+
