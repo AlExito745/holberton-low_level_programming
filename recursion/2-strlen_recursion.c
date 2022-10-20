12813afd676ff168989fd3770d3cb4afdf587300
AlExito745 <alxtinoco1988@gmail.com>
2022-10-19 20:41:11 -0700

    length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - lenght of a string
+  *@s: pointer to string
+  *return: nothing
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
